582024a31243ba11725bee6c2f0aec7ae708ffc2
LeeJams <woals214@gmail.com>
2023-08-27 21:41:59 +0900

    초안

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./views/Home.jsx";
 import Layout from "./components/layouts/Layout.jsx";
+import Description from "./views/Description.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/docs/:id",
+        element: <Description />,
+      },
+    ],
   },
 ]);
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,70 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigator = useNavigate();
+
+  const moveToDocs = (id) => {
+    navigator("/docs/" + id);
+  };
+
   return (
     <div>
-      <h1>Home</h1>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            초전도체
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            onClick={() => {
+              moveToDocs("superconductor");
+            }}
+          >
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            ChatGPT
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            onClick={() => {
+              moveToDocs("chatgpt");
+            }}
+          >
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            삼중수소
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            size="small"
+            onClick={() => {
+              moveToDocs("triplehydrogen");
+            }}
+          >
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
